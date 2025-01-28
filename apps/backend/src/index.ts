c6fafe58fdcecc686e248f356b70d24ebd3f0c1f
MizuYaYa <106097784+MizuYaYa@users.noreply.github.com>
2025-01-28 11:02:08 +0900

    💥 CloudFlare Workers から nodejsで動かす

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,9 +1,27 @@
-import { Hono } from 'hono'
+import { readFileSync } from "node:fs";
+import { createSecureServer } from "node:http2";
+import { serve } from "@hono/node-server";
+import { createNodeWebSocket } from "@hono/node-ws";
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const { injectWebSocket, upgradeWebSocket } = createNodeWebSocket({ app });
 
-export default app
+app.get("/", c => {
+  return c.text("Hello Hono!");
+});
+
+const port = 3000;
+console.log(`Server is running on http://localhost:${port}`);
+
+const server = serve({
+  fetch: app.fetch,
+  port,
+  createServer: createSecureServer,
+  serverOptions: {
+    key: readFileSync("localhost-privkey.pem"),
+    cert: readFileSync("localhost-cert.pem"),
+  },
+});
+injectWebSocket(server);
