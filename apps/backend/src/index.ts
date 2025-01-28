b4ece0d33e9c9b0e8fa29ea03ae33d318339b3bf
MizuYaYa <106097784+MizuYaYa@users.noreply.github.com>
2025-01-28 19:53:48 +0900

    🚧 webRTC確立のためのwebsocket通信を実装中
    何故か繋がらない...honoでの使い方が間違ってるのか？

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -3,17 +3,41 @@ import { createSecureServer } from "node:http2";
 import { serve } from "@hono/node-server";
 import { createNodeWebSocket } from "@hono/node-ws";
 import { Hono } from "hono";
+import { compress } from "hono/compress";
+import { etag } from "hono/etag";
+import { secureHeaders } from "hono/secure-headers";
+import { logger } from "hono/logger";
 
 const app = new Hono();
 
 const { injectWebSocket, upgradeWebSocket } = createNodeWebSocket({ app });
 
-app.get("/", c => {
-  return c.text("Hello Hono!");
-});
+app.use(compress(), etag(), secureHeaders(), logger());
+
+app.get(
+  "/host",
+  upgradeWebSocket(c => {
+    console.log("WebSocket connection established");
+    return {
+      onMessage(event, ws) {
+        console.log(`Message from client: ${event.data}`);
+        ws.send("Hello from server!");
+      },
+      onClose: () => {
+        console.log("Connection closed");
+      },
+      onOpen: () => {
+        console.log("Connection opened");
+      },
+      onError: error => {
+        console.error(`WebSocket error: ${error}`);
+      }
+    };
+  }),
+);
 
 const port = 3000;
-console.log(`Server is running on http://localhost:${port}`);
+console.log(`Server is running on https://localhost:${port}`);
 
 const server = serve({
   fetch: app.fetch,
---
+++ apps/web/src/components/Sender.tsx
@@ -0,0 +1,48 @@
+import { Dropzone } from "@yamada-ui/dropzone";
+import { Center, FormatByte, Heading, Text } from "@yamada-ui/react";
+import { useEffect, useState } from "react";
+
+export default function Sender() {
+  const [wsState, setWsState] = useState("");
+
+  useEffect(() => {
+    try {
+      const ws = new WebSocket("ws://localhost:3000/host");
+      ws.addEventListener("open", () => {
+        ws.send("Hello, WebSocket!");
+        setWsState(ws.readyState.toString());
+        console.log("Connection opened");
+      });
+      ws.addEventListener("message", event => {
+        console.log(`Message from server: ${event.data}`);
+      });
+      ws.addEventListener("close", () => {
+        console.log("Connection closed");
+      });
+      ws.addEventListener("error", error => {
+        console.error(`event WebSocket error: ${error}`);
+      });
+    } catch (error) {
+      console.error(`WebSocket error: ${error}`);
+    }
+  }, []);
+
+  const maxTransferSize = 1000 ** 3;
+  const maxFiles = 5;
+
+  return (
+    <Center>
+      <Dropzone multiple maxSize={maxTransferSize} maxFiles={5} mx="3xl" size="sm">
+        <Text fontSize="xl">
+          ドラッグ&ドロップかクリックしてファイルを追加
+          <br />
+          最大
+          <FormatByte value={maxTransferSize} />
+          <br />
+          {maxFiles}ファイルまで
+        </Text>
+      </Dropzone>
+      <Heading>WebSocket State: {wsState}</Heading>
+    </Center>
+  );
+}
