1009e20de2a66a1a570a93649b2df7ecec68f453
MizuYaYa <106097784+MizuYaYa@users.noreply.github.com>
2025-01-28 11:03:19 +0900

    ✨ react-routerを追加し、アプリケーションのルーティングを設定

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,13 +1,18 @@
+import { UIProvider } from "@yamada-ui/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { UIProvider } from "@yamada-ui/react";
+import { BrowserRouter, Route, Routes } from "react-router";
 import App from "./App.tsx";
 
 // biome-ignore lint/style/noNonNullAssertion:
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <UIProvider>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </BrowserRouter>
     </UIProvider>
   </StrictMode>,
 );
