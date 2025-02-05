6b08c6b0acf27d40f3bf360cbff797961f1b0781
MizuYaYa <106097784+MizuYaYa@users.noreply.github.com>
2025-02-05 22:55:59 +0900

    🎨 フォルダ構成を整理し、レイアウトと404ページを追加

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,9 +1,13 @@
-import About from "@/components/About.tsx";
 import { UIProvider } from "@yamada-ui/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
-import App from "./App.tsx";
+
+import AppLayout from "@/layouts/AppLayout";
+import About from "@/pages/About";
+import NotFound from "@/pages/NotFound";
+import Receiver from "@/pages/Receiver";
+import Sender from "@/pages/Sender";
 
 // biome-ignore lint/style/noNonNullAssertion:
 createRoot(document.getElementById("root")!).render(
@@ -11,8 +15,11 @@ createRoot(document.getElementById("root")!).render(
     <UIProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/connect/:roomId" element={<App />} />
+          <Route path="/" element={<AppLayout />}>
+            <Route path="/" element={<Sender />} />
+            <Route path="/connect/:roomId" element={<Receiver />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
           <Route path="/about" element={<About />} />
         </Routes>
       </BrowserRouter>
