8e037a8fd3b7a3e13c838b4ff90e1ab3e6087ce9
MizuYaYa <106097784+MizuYaYa@users.noreply.github.com>
2025-03-04 16:38:56 +0900

    ファイル送信/受信の進捗度を可視化 (#12)
    * feat: 🍻 Sender側に送信の進捗度を表示
    
    * feat: ✨ Receiver側に受信の進捗を表示

--- apps/web/src/components/FileList.tsx
+++ apps/web/src/components/FileList.tsx
@@ -1,14 +1,17 @@
 import { TrashIcon } from "@yamada-ui/lucide";
 import {
+  Box,
   Card,
   CardBody,
   CardFooter,
   CardHeader,
   Center,
+  Flex,
   For,
   FormatByte,
   Heading,
   IconButton,
+  Progress,
   ScrollArea,
   Spacer,
   Stack,
@@ -17,12 +20,15 @@ import {
 } from "@yamada-ui/react";
 import type { Dispatch, SetStateAction } from "react";
 
+import type { QueuedFile, Receiver } from "@/pages/Sender";
+
 export type SenderFilesProps = {
-  files: File[];
-  setFiles: Dispatch<SetStateAction<File[]>>;
+  files: QueuedFile[];
+  setFiles: Dispatch<SetStateAction<QueuedFile[]>>;
+  receivers: Receiver[];
 };
 
-export default function FileList({ files, setFiles }: SenderFilesProps) {
+export default function FileList({ files, setFiles, receivers }: SenderFilesProps) {
   return (
     <ScrollArea
       type="always"
@@ -34,30 +40,44 @@ export default function FileList({ files, setFiles }: SenderFilesProps) {
       overflowX="hidden"
     >
       <For each={files} fallback={<Center>ファイルがありません</Center>}>
-        {(file, i) => (
-          <Card key={i}>
-            <CardHeader>
-              <Tooltip label={file.name} placement="top" fontSize="xs">
-                <Heading fontSize="sm" overflow="hidden" textOverflow="ellipsis" lineClamp={2}>
-                  {file.name}
-                </Heading>
-              </Tooltip>
-            </CardHeader>
-            <CardBody>
-              <Text>{}</Text>
-            </CardBody>
-            <CardFooter>
-              <FormatByte value={file.size} />
-              <Spacer />
-              <IconButton
-                icon={<TrashIcon />}
-                onClick={() => {
-                  setFiles((prev) => prev.filter((f) => f !== file));
-                }}
-              />
-            </CardFooter>
-          </Card>
-        )}
+        {({ file, start, end }, i) => {
+          const totalSentBytes = receivers
+            .map((receiver) => receiver.filesSendState[file.name]?.sentByte || 0)
+            .reduce((a, c) => a + c, 0);
+          const sentPercentage =
+            receivers.length > 0 ? Number((((totalSentBytes / file.size) * 100) / receivers.length).toFixed(0)) : 0;
+
+          return (
+            <Card key={i}>
+              <CardHeader>
+                <Tooltip label={file.name} placement="top" fontSize="xs">
+                  <Heading fontSize="sm" overflow="hidden" textOverflow="ellipsis" lineClamp={2}>
+                    {file.name}
+                  </Heading>
+                </Tooltip>
+              </CardHeader>
+              <CardBody>
+                <Box w="full">
+                  <Flex fontSize="sm" justifyContent="space-between">
+                    <Text> {sentPercentage}%</Text>
+                    <Text>{start && end ? ((end.getTime() - start.getTime()) / 1000).toFixed(1) : "???"}秒経過</Text>
+                  </Flex>
+                  <Progress value={sentPercentage} rounded="sm" />
+                </Box>
+              </CardBody>
+              <CardFooter>
+                <FormatByte value={file.size} />
+                <Spacer />
+                <IconButton
+                  icon={<TrashIcon />}
+                  onClick={() => {
+                    setFiles((prev) => prev.filter((f) => f.file !== file));
+                  }}
+                />
+              </CardFooter>
+            </Card>
+          );
+        }}
       </For>
     </ScrollArea>
   );
